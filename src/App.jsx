847d43f429af30eff2bae987d355ed72c143b36f
Satheesh-Baabu <satheeshbaabum@gmail.com>
2024-12-26 12:00:37 +0530

    merging

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import QRgenerator from './components/QRgenerator';
 import TableList from './components/TableList';
 import FoodList from './components/FoodList';
 import AddFood from './components/AddFood';
+import ShowFood from './User/ShowFood';
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [headerTitle, setHeaderTitle] = useState("Welcome");
-  const menuItems = ["Home", "QR Code Generator","Table List","Food List","Add Food"];
+  const menuItems = ["Home", "QR Code Generator","Table List","Food List","Add Food","Show Food"];
   const cards = ["Card 1", "Card 2", "Card 3"];
-  const links=["/","qrgenerator","tablelist","foodlist","addfood"]
+  const links=["/","qrgenerator","tablelist","foodlist","addfood","showfood"]
 
   const handleMenuClick = (item) => {
     setHeaderTitle(item);
@@ -21,7 +22,6 @@ function App() {
   return (
     <Router>
       <div className="flex h-screen bg-gray-100">
-        {/* Sidebar */}
         <Sidebar
           isOpen={isSidebarOpen}
           toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)}
@@ -31,10 +31,8 @@ function App() {
         />
 
         <div className="flex flex-col flex-1 overflow-hidden">
-          {/* Header */}
           <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} title={headerTitle} />
 
-          {/* Main Content */}
           <main className="flex-1 overflow-y-auto p-4">
             <Routes>
               <Route path="/" element={<DashboardContent cards={cards} />} />
@@ -42,6 +40,7 @@ function App() {
               <Route path="/tablelist" element={<TableList/>}/>
               <Route path="/foodlist" element={<FoodList/>}/>
               <Route path="/addfood" element={<AddFood/>}/>
+              <Route path="/showfood" element={<ShowFood/>}/>
             </Routes>
           </main>
         </div>
